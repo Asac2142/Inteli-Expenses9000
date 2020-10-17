ed689f5ec7fbd54caad81370c71e21730400f83a
Andres Alban <bethesdas101@gmail.com>
2020-10-17 13:10:15 -0500

    Refactoring readStream for expenses file

--- src/file.utils.js
+++ src/file.utils.js
@@ -2,10 +2,7 @@ const fs = require('fs');
 const readline = require('readline');
 
 const inputFile = readline.createInterface({
-    input: fs.createReadStream('./assets/expenses.txt'),
-    output: process.stdout,
-    terminal: false
+    input: fs.createReadStream('./assets/expenses.txt')
 });
 
-//export default inputFile;
 module.exports = inputFile;
