fa5241e97597045c08e8f04c4ac6b99d9af2f4e2
Andres Alban <bethesdas101@gmail.com>
2020-10-17 14:18:29 -0500

    Deleting integration test file

--- spec/index.it.spec.js
+++
@@ -1,9 +0,0 @@
-const expenseApp = require('../src/index');
-const testFile = require('../src/file.utils');
-
-describe("Integration test for Big Big Band Ecuatoriano app", () => {   
-    it("should not keep reading the file once it reads a zero character", () => {
-        expenseApp.setExpensesData(testFile);          
-        expect(expenseApp.balancePerTrip.length).toEqual(5);
-    });
-});
